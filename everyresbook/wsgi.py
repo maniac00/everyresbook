f4d4955e7c0b29fb6b05719e3da3398f338b5cec
mario <mario@marioui-MacBook-Pro.local>
2018-03-19 12:56:40 +0900

    모두의맛집 생성

---
+++ everyresbook/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for everyresbook project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "everyresbook.settings")
+
+application = get_wsgi_application()
